c68f4033722c597c614be9186852758d366294f3
Brandon May <battleb2014@gmail.com>
2021-08-03 18:51:34 -0400

    almost done

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const body = document.querySelector('body');
 window.addEventListener('load', (event) => body.style.textAlign = 'center');
 
  // Toggle background color with first img mouse on and mouse of
- const firstImg = document.querySelector('header img');
+ const firstImg = document.querySelector('.intro img');
  firstImg.addEventListener('mouseover', event => {
      body.style.backgroundColor = 'skyblue';
      event.stopPropagation();
@@ -22,3 +22,26 @@ window.addEventListener('load', (event) => body.style.textAlign = 'center');
      }
  }
  document.addEventListener('keydown', enter);
+
+const nav = document.querySelector('.nav');
+const home = nav.firstElementChild;
+
+home.addEventListener('click', event => {
+    alert('The bus is broke down.  No ride home!');
+    event.preventDefault();
+});
+
+document.addEventListener('scroll', event => body.style.color = 'blue');
+
+function zoom(event) {
+    event.preventDefault();
+
+    let scale = 1;
+    scale += event.deltaY * -0.01;
+    img2.style.transform = `scale(${scale})`;
+}
+
+const img2 = document.querySelector('.content-section .img-content img');
+img2.addEventListener('wheel', zoom);
+
+const img3 = document.querySelector('.inverse-content img');
